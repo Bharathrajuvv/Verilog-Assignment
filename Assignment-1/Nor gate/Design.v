d9bacaea0f81a69d10b35a7a2f31f3a1dd94d0cf
Bharathrajuvv <rajubharath542@gmail.com>
2025-06-05 10:29:41 +0530

    Create Design.v

---
+++ Assignment-1/Nor gate/Design.v
@@ -0,0 +1,16 @@
+// Design code for Nor gate usign Switch-level and Gate level
+module nor_gate(input a,b, output y);
+  wire w;
+  supply1 vdd;
+  supply0 gnd;
+  pmos p1(w,vdd,a);
+  pmos p2(y,w,b);
+  nmos n1(y,gnd,a);
+  nmos n2(y,gnd,b);
+endmodule
+
+module nor_gate1(input a,b, output y);
+  nor g1 (y,a,b);
+endmodule
+
+  
